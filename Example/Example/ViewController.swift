92e7dec5c125dabc7cb96902db23222790f34ca2
nika <puhohopla@yandex.ru>
2024-02-10 19:18:49 +0300

    1

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nika on 2/10/24.
+//
+
+import UIKit
+import StyleRuNetSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        a.printLetter()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StyleRuNetSDK/Sources/StyleRuNetSDK/StyleRuNetSDK.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public final class a {
+    public static func printLetter() {
+        print("aaaa")
+    }
+}
