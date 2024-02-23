d0a7e4880449069ea5ded23352ed93aa18bbcc2b
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-02-23 19:14:36 +0300

    Created a SRNSession class
    This version has a singleton implementation, a basic init and public methods for creating data tasks.

--- StyleRuNetSDK/Sources/StyleRuNetSDK/SRNSession.swift
+++ StyleRuNetSDK/Sources/StyleRuNetSDK/SRNSession.swift
@@ -1,1 +1,28 @@
 import Foundation
+
+class SRNSession {
+    // MARK: - Properties
+    
+    static let shared = SRNSession()
+    private let session: URLSession
+    
+    // MARK: - Initialization
+    
+    private init() {
+        let configuration = URLSessionConfiguration.default
+        session = URLSession(configuration: configuration)
+    }
+    
+    // MARK: - Public Methods
+    
+    // Creates a data task with the given request and returns it
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return session.dataTask(with: request, completionHandler: completionHandler)
+    }
+    
+    // Creates a data task with the given URL and returns it
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return session.dataTask(with: url, completionHandler: completionHandler)
+    }
+}
+
